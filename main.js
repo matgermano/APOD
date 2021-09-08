15bc714fe88b396b1b42c5d17a5bbce75fd45da2
matgermano <88158544+matgermano@users.noreply.github.com>
2021-09-08 12:01:39 -0300

    Inserção de footer | Estilização de página | Correção de bugs no JS   -   falta centralizar o '.fieldset'

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 $('button').on('click', async function (event) {
     event.preventDefault();
-    $('#main-texto').css("display", "none");
+    $('#inicio').css("display", "none");
     let valueData = $('#date').val();
     let url = `https://api.nasa.gov/planetary/apod?api_key=QRaDd5HWwVtjNo83M6DhwSdhxw9M2Ibxj798RL5w&date=${valueData}`;
-    let apiResponse = await fetch(url).then(response => response.json());
-    $('body').css("background-image", `url(${apiResponse.url})`);
+    let api = await fetch(url).then(response => response.json());
+    $('body').css("background-image", `url(${api.url})`);
     $('body').css("background-repeat", "no-repeat");
     $('body').css("background-size", "cover");
     $('#corpo').css('display', 'block');
-    $('#titulo').html(`${apiResponse.title}`);
-    $('#texto').html(`(${apiResponse.explanation})`);
-
+    $('#titulo').html(`${api.title}`);
+    $('#texto').html(`(${api.explanation})`);
     console.log(event)
     console.log(apiResponse)
     console.log(inputDate)
